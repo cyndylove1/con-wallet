7e92d853a94a2c57bbddde0b07b7833fdb00f9fb
cynthia <nwekecynthia2015@gmail.com>
2023-10-29 21:46:52 -0700

    intial commit

--- app/Mobile/page.jsx
+++ app/Mobile/page.jsx
@@ -5,38 +5,38 @@ import {GrMenu} from 'react-icons/gr'
 import {FaGoogleWallet} from 'react-icons/fa6'
 import Link from 'next/link'
 export default function Mobile() {
-    return(
-        <div>
+//     return(
+//         <div>
 
-        <main>
-            <div><GrMenu/></div>
+//         <main>
+//             <div><GrMenu/></div>
            
             
-            <div className='logo1'>
-                <h2><span className='fa'><FaGoogleWallet/></span>HEYWALLET</h2>
+//             <div className='logo1'>
+//                 <h2><span className='fa'><FaGoogleWallet/></span>HEYWALLET</h2>
 
-           </div>
+//            </div>
 
 
-           <nav class="mobile">
-               <ul>
-                   <li><a href='#'>Home</a></li>
-                   <li><a href='#'>About</a></li>
-                   <li><a href='#'>Services</a></li>
-                   <li><a href='#'>WhyUs</a></li>
-                   <li><a href='#'>Team</a></li>
-                   <li><a href='#'>Testimonial</a></li>
+//            <nav class="mobile">
+//                <ul>
+//                    <li><a href='#'>Home</a></li>
+//                    <li><a href='#'>About</a></li>
+//                    <li><a href='#'>Services</a></li>
+//                    <li><a href='#'>WhyUs</a></li>
+//                    <li><a href='#'>Team</a></li>
+//                    <li><a href='#'>Testimonial</a></li>
                                                            
-                   <Link href="/form">
-                       <button className='btn'>Login</button>
-                   </Link>
+//                    <Link href="/form">
+//                        <button className='btn'>Login</button>
+//                    </Link>
                    
-               </ul>
+//                </ul>
 
-           </nav>
+//            </nav>
        
-   </main> 
+//    </main> 
 
-        </div>
-    )
+//         </div>
+//     )
 }
